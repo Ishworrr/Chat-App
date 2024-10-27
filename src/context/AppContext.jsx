7b852847d6b37dd170027b4b9217111caf0a60bf
Ishwor <ishwo5678@gmail.com>
2024-10-27 21:32:37 +1100

    second

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,25 +1,53 @@
 //for userdata and chat data
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { db } from "../config/firebase";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-  const [userData, setUserData] = useState(null); //userdata
-  const [chatData, setChatData] = useState(null); //chatdtata
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState(null); //store userdata
+  const [chatData, setChatData] = useState(null); //store chatdtata
 
   const loadUserData = async (uid) => {
     try {
       const userRef = doc(db, "users", uid);
       const userSnap = await getDoc(userRef);
-      console.log(userSnap);
+      const userData = userSnap.data();
+      // console.log(userSnap);
+      // console.log(userData);
+      setUserData(userData);
+      if (userData.avatar && userData.name) {
+        //if available userData
+        navigate("/chat");
+        console.log(user);
+      } else {
+        navigate("/profile");
+      }
+      await updateDoc(userRef, {
+        // lastSeen: new Date().toISOString(),
+        lastSeen: Date.now(),
+      });
+      setInterval(async () => {
+        if (auth.chatUser) {
+          //if user is authenticated get time every 1min/ registered
+
+          await updateDoc(userRef, {
+            lastSeen: Date.now(),
+          });
+        }
+      }, 60000);
     } catch (error) {}
   };
   const value = {
+    //passes here to access in any components
     userData,
     setUserData,
     chatData,
     setChatData,
+    loadUserData,
   };
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,12 +1,69 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./ProfileUpdate.css";
 import assets from "../../assets/assets";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, db } from "../../config/firebase";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 const ProfileUpdate = () => {
+  const navigate = useNavigate(); //last,for else
+
   const [image, setImage] = useState(false);
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState(""); //initialize bio with empty string
+  const [uid, setUid] = useState("");
+  const [prevImage, setPrevImage] = useState("");
+
+  const profileUpdate = async (event) => {
+    event.preventDefault();
+    try {
+      if (!prevImage && image) {
+        //if prevImage is not available and user has not selected ny new image
+        toast.error("Upload Profile Picture");
+      }
+      const docRef = doc(db, "users", uid);
+      if (image) {
+        //if user has image selected
+        const imageUrl = await upload(image);
+        setPrevImage(imageUrl);
+        await updateDoc(docRef, {
+          avatar: imageUrl,
+          bio: bio,
+          name: name,
+        });
+      } else {
+      }
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        setUid(user.uid);
+
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.data().name) {
+          //if user have all these they will set them in this state. otherwise they will be empty
+          //if user login in they will be sent here otherwise else
+          setName(docSnap.data().name);
+        }
+        if (docSnap.data().bio) {
+          setBio(docSnap.data().bio);
+        }
+        if (docSnap.data().avatar) {
+          setPrevImage(docSnap.data().avatar); //if user have avatar property available
+        }
+      } else {
+        navigate("/");
+      }
+    });
+  });
   return (
     <div className="profile">
       <div className="profile-container">
-        <form>
+        <form onSubmit={profileUpdate}>
           <h3>Profile Details</h3>
           <label htmlFor="avatar">
             <input
@@ -22,8 +79,19 @@ const ProfileUpdate = () => {
             />
             upload profile image
           </label>
-          <input type="text" placeholder="Your name" required />
-          <textarea placeholder="Write profile bio" required></textarea>
+          <input
+            onChange={(e) => setBio(e.target.value)}
+            value={name}
+            type="text"
+            placeholder="Your name"
+            required
+          />
+          <textarea
+            onChange={(e) => setBio(e.target.bio)}
+            value={bio}
+            placeholder="Write profile bio"
+            required
+          ></textarea>
           <button type="submit">Save</button>
         </form>
         <img
