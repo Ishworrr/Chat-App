d94d49c82ba643654f8aae1f0b3a6f5f48d53fdc
Ishwor <ishwo5678@gmail.com>
2024-11-17 17:45:11 +1100

    test-browser

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import dns from "dns";
+import { defineConfig } from "vite";
+
+// localhost part
+dns.setDefaultResultOrder("verbatim");
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+});
